c0898bf4b4d13f04b196a859edb1fc82f3710103
HMIMID <ahmedhmimid177@gmail.com>
2024-11-25 18:38:25 +0100

    feat: update file

--- routes/api.js
+++ routes/api.js
@@ -134,6 +134,7 @@ module.exports = function (app) {
           try {
             await Issue.findByIdAndUpdate({_id: req.body._id},
               {
+                _id: req.body._id,
                 assigned_to: req.body.assigned_to,
                 status_text: req.body.status_text,
                 open: req.body.open === 'false' ? false : true,
